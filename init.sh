7619aa69dbda5830bbb53a9fa13d58ffed8c03bd
zerint <66475632+zerintg@users.noreply.github.com>
2022-05-10 16:06:26 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,9 +4,8 @@ echo "[DEBUG]"
 date
 printenv                                                   
 tree || true
-echo '\n\n\n'
+echo "\n\n\n"
 
 echo "[CLEANUP]"
 docker ps -q | grep -qE '*' && docker stop $(docker ps -q)
 docker ps -aq | grep -qE '*' && docker rm $(docker ps -aq)
-echo '\n\n\n'
