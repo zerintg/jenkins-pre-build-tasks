2c426fa17f2b4ba10fc8bab63749fc4b88cdd537
zerint <66475632+zerintg@users.noreply.github.com>
2022-05-10 15:14:09 +0200

    Update cleanup.sh

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+tree || true                         # List directory structure
 rm -rf ./*                           # Cleaning working directory
 docker stop $(docker ps -q) || true  # Stopping docker containers
 docker rm $(docker ps -qa) || true   # Removing docker containers
