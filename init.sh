81cd607aac58b15bad9a07bbb149aaf4e1745156
zerint <66475632+zerintg@users.noreply.github.com>
2022-05-12 12:40:55 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -10,16 +10,3 @@ if which docker; then
   docker ps -q | grep -qE '.+' && docker stop $(docker ps -q)
   docker ps -aq | grep -qE '.+' && docker rm $(docker ps -aq)
 fi
-
-WS="$(pwd | grep -oE '.*/remote-root/workspace')"
-if [[ "$WS" ]]; then
-  echo "[WORKSPACE CLEANUP]"
-  WS="$WS"
-  rm -vrf $WS/*
-else
-  echo "\
-Not building in the workspace! If you don't know what \
-caused this error, please contact the Jenkins admin.  \
-"
-  exit 1
-fi
